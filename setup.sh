26d318528d438da77b2ac1772da242622995e05e
Derek Spiner <dspiner@dspiner-ltm.internal.salesforce.com>
2018-09-25 20:12:53 -0700

    more changes

--- setup.sh
+++ setup.sh
@@ -7,9 +7,7 @@ oktaLocal="/Users/$username/.okta"
 
 cd $oktaLocal
 
-$("https://raw.githubusercontent.com/Econis2/aws-cli-okta/master/components/bash-functions" --output bash_functions)
-
-#mv bash_functions.sh bash_functions
+$(curl https://raw.githubusercontent.com/Econis2/aws-cli-okta/master/components/bash-functions --output bash_functions)
 
 $(curl https://raw.githubusercontent.com/Econis2/aws-cli-okta/master/components/main.py --output main.py)
 
