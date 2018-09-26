6bc2279945c08759c276f44a2205ca6ed7791dd5
Derek Spiner <dspiner@dspiner-ltm.internal.salesforce.com>
2018-09-25 20:16:24 -0700

    creating bash functions locally instead of faild download

--- setup.sh
+++ setup.sh
@@ -7,7 +7,28 @@ oktaLocal="/Users/$username/.okta"
 
 cd $oktaLocal
 
-$(curl https://raw.githubusercontent.com/Econis2/aws-cli-okta/master/components/bash-functions --output bash_functions)
+cat > $oktaLocal/bash_functions <<-EOM
+#!bin/bash
+
+function aws-okta {
+
+    paramFirst=$0
+
+    user=$(whoami)
+    #Sets the account profile determines Account Status
+    result=$(python /Users/$user/.okta/main.py $@)
+
+    #Renew Token
+    if (( $? == 0 )); then
+        withokta aws $@
+    
+    #Use Existing
+    elif (( $? == 1 )); then
+        aws --profile $@
+    fi
+
+}
+EOM
 
 $(curl https://raw.githubusercontent.com/Econis2/aws-cli-okta/master/components/main.py --output main.py)
 
