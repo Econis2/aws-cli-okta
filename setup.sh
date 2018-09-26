2d50263c43875d18cf9555e8f60fb78481a53f68
Derek Spiner <dspiner@dspiner-ltm.internal.salesforce.com>
2018-09-25 19:02:13 -0700

    added setup script

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!bin/bash
+
+echo "Getting Required Files"
+
+username=$(whoami)
+oktaLocal="/Users/$username/.okta"
+
+cd $oktaLocal
+
+curl https://raw.githubusercontent.com/Econis2/aws-cli-okta/master/components/main.py
+
+curl https://raw.githubusercontent.com/Econis2/aws-cli-okta/master/components/newProfile.py
+
+echo "Creating required Directories"
+
+mkdir "$oktaLLocal\awsProfiles"
+
+exit 0
