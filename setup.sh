f813f674ab7dadd422b813851a9b3cd4699e0b42
Derek Spiner <dspiner@dspiner-ltm.internal.salesforce.com>
2018-10-10 13:19:56 -0700

    adjusted setup script to point to the correct branch

--- setup.sh
+++ setup.sh
@@ -7,9 +7,9 @@ oktaLocal="/Users/$username/.okta"
 
 cd $oktaLocal
 
-$(curl https://raw.githubusercontent.com/Econis2/aws-cli-okta/master/components/bash_functions --output bash_functions)
+$(curl https://raw.githubusercontent.com/Econis2/aws-cli-okta/simple/components/bash_functions --output bash_functions)
 
-$(curl https://raw.githubusercontent.com/Econis2/aws-cli-okta/master/components/main.py --output main.py)
+$(curl https://raw.githubusercontent.com/Econis2/aws-cli-okta/simple/components/main.py --output main.py)
 
 sudo chmod +x main.py
 
